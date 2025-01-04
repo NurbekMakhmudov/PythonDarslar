f3ca4bf1d6aa3cc56c0aaa2574f41f960f83463f
Nurbek Makhmudov <nurbek.makhmudov.93@gmail.com>
2025-01-04 14:12:21 +0500

    added two lessons

---
+++ 01-PRINT/basic.py
@@ -0,0 +1,15 @@
+
+
+print('Men "Dell" markasidagi  noutbuk sotib oldim')
+print("Men 'Dell' markasidagi noutbuk sotib oldim")
+
+print("""
+         
+Odami ersang, demagil odami,
+Oniki, yo'q xalq g'amidin g'ami
+
+""")
+
+print("Odami ersang, demagil odami,\nOniki, yo'q xalq g'amidin g'ami")
+
+print('\n\n\nOdami ersang, demagil odami, \nOniki, yo\'q xalq g\'amidin g\'ami')
---
+++ 02-ARIFMETIK-AMALLAR/mian.py
@@ -0,0 +1,11 @@
+print(2 + 4 * 2)
+
+print(19 / 5)
+
+print(20 / 5)
+
+print(16 // 4)
+
+print(10 // 3)
+
+print(2 ** 4)
